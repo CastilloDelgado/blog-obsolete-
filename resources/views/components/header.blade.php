bd30219a1cb61b4131713b844feb34a088536a5c
Marco Castillo <marco.castillo@emergys.com>
2023-02-04 18:12:56 -0600

    Adding the main layout and auth

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<div class="flex justify-center py-10">
+  <div class="md:w-1/2 px-3">
+
+    <div class="flex flex-row justify-center font-bold mb-6 border-b-2 border-black pb-6">
+      <h1 class="text-center text-7xl">Life & live music</h1>
+      <p class="font-sans font-light ml-1">(blog)</p>
+    </div>
+
+    <h2 class="text-center text-5xl font-light">
+      <b class="font-bold">Buenas</b> historias y fotos <b class="font-bold">horribles</b> de momentos <b
+        class="font-bold">inolvidables</b>
+    </h2>
+  </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="md:flex md:justify-between md:items-center bg-gray-100 p-5">
+  <div>
+    <a href="/">
+      <div class="flex flex-row ">
+        <p class="text-2xl font-bold">Life & life music</p>
+        <small class="font-light font-sans ml-1">(blog)</small>
+      </div>
+      {{-- <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16"> --}}
+    </a>
+  </div>
+
+  <div class="mt-8 md:mt-0 flex items-center">
+
+    <a href="/register" class="text-sm font-sans hover:underline">Registrate</a>
+    <a href="/login" class="text-sm ml-4 font-sans hover:underline">Iniciar Sesión</a>
+
+    <a href="#newsletter" class="font-sans bg-black ml-3 rounded text-sm font-semibold text-white py-2 px-3">
+      Newsletter
+    </a>
+  </div>
+</nav>
