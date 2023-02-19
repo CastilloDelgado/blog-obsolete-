c83750210cf7b6bf1fad043d1b196944753e1c7f
Marco Castillo <marcocastillo1997@gmail.com>
2023-02-19 11:21:51 -0600

    Changing list colors

--- resources/views/components/posts/recent-posts-list.blade.php
+++ resources/views/components/posts/recent-posts-list.blade.php
@@ -6,7 +6,7 @@
   <x-posts.post-small-list :posts='[
       [
         "dir" => "storage/images/header-1.jpeg",
-        "bg" => "bg-red-100",
+        "bg" => "bg-green-100",
         "artist" => "Amyl and the sniffers",
         "genres" => ["Rock", "Punk", "Neopunk"],
         "concert_date" => "Enero 12 del 2023",
@@ -16,7 +16,7 @@
       ],
       [
         "dir" => "storage/images/header-2.jpeg",
-        "bg" => "bg-red-200",
+        "bg" => "bg-green-200",
         "artist" => "Amyl and the sniffers",
         "genres" => ["Rock", "Punk", "Neopunk"],
         "concert_date" => "Enero 12 del 2023",
@@ -26,7 +26,7 @@
       ],
       [
         "dir" => "storage/images/header-3.jpeg",
-        "bg" => "bg-red-300",
+        "bg" => "bg-green-300",
         "artist" => "Amyl and the sniffers",
         "genres" => ["Rock", "Punk", "Neopunk"],
         "concert_date" => "Enero 12 del 2023",
